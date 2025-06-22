565fba98c2e52da32eb623f9f1013567b03b3e0f
Leonardo <leodeveloper12@gmail.com>
2025-06-22 09:51:49 -0300

    Terminando o Header

--- classroom/src/App.tsx
+++ classroom/src/App.tsx
@@ -2,10 +2,15 @@ import { Header } from "./components/Header"
 import styles from "./App.module.css"
 
 export function App() {
+
+  function handleRestartGame() {
+    alert("Reiniciar o jogo")
+  }
+
   return (
     <div className={styles.container}>
       <main>
-        <Header />
+        <Header current={5} max={11} onRestart={handleRestartGame} />
       </main>
     </div>
   ) 
--- classroom/src/components/Header/index.tsx
+++ classroom/src/components/Header/index.tsx
@@ -1,11 +1,28 @@
 import styles from "./styles.module.css"
 import logo from "../../assets/logo.png"
+import restart from "../../assets/restart.svg"
+// import { type } from './../../utils/words';
 
+type Props = {
+    current: number; // Número de tentativas
+    max: number; // Número máximo de tentativas
+    onRestart: () => void // Função para retornar a execução
+}
 
-export function Header() {
+export function Header({ current, max, onRestart }: Props) {
     return (
         <div className={styles.container}>
             <img src={logo} alt="Logo" />
+
+            <header>
+                <span>
+                    <strong>{current}</strong> de {max} tentativas
+                </span>
+
+                <button type="button" onClick={onRestart}>
+                    <img src={restart} alt="Ícone de reiniciar" />
+                </button>
+            </header>
         </div>
     )
 }
